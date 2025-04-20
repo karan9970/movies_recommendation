4f789d50322f6b10927458fc4a127f818049a66c
Desai Karan Sachin <45914471+karan9970@users.noreply.github.com>
2025-04-20 18:09:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np
 import pandas as pd
-import warnings
+import warnings 
 warnings.filterwarnig('ignore')
 from sklearn.metrics.pairwise import cosine_similarity
 from difflib import get_close_matches  # Import for handling close matches
